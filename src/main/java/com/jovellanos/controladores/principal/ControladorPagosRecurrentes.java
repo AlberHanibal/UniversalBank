36b1732d4136595f9c17538577b82e0ef4e6b743
Santiago <aguilanegra_moha@hotmail.com>
2024-05-19 19:14:59 +0200

    Primera version de Pagos Recurrentes

--- src/main/java/com/jovellanos/controladores/principal/ControladorPagosRecurrentes.java
+++ src/main/java/com/jovellanos/controladores/principal/ControladorPagosRecurrentes.java
@@ -1,5 +1,160 @@
 package com.jovellanos.controladores.principal;
+import java.util.Calendar;
+import java.util.Date;
+
+import com.jovellanos.App;
+import com.jovellanos.ControladorMongoDB;
+import com.jovellanos.modelo.Cuenta;
+import com.jovellanos.modelo.GastosPeriodicos;
+import com.jovellanos.modelo.Usuario;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class ControladorPagosRecurrentes {
+    private Usuario usuario = App.getUsuario();
+    private Cuenta cuenta = App.getCuenta();
 
+    @FXML
+    private TextField txtCantidadTotal;
+
+    @FXML
+    private TextField txtInteresAnual;
+
+    @FXML
+    private TextField txtPlazos;
+
+    @FXML
+    private TextField txtPlazosDinero;
+
+    @FXML
+    private DatePicker dtpFechaInicio;
+
+    @FXML 
+    private ComboBox<String> cmbPlazos;
+
+    @FXML 
+    private ComboBox<String> cmbTipo;
+
+    @FXML
+    private TableView<GastosPeriodicos> tblPagosRecorrentes;
+
+    @FXML
+    private TableColumn<GastosPeriodicos, Double> colCantidadTotal;
+
+    @FXML
+    private TableColumn<GastosPeriodicos, Double> colCantidadPagada;
+
+    @FXML
+    private TableColumn<GastosPeriodicos, Double> colCantidadRestante;
+
+    @FXML
+    private TableColumn<GastosPeriodicos, Double> colDineroPlazo;
+
+    @FXML
+    private TableColumn<GastosPeriodicos, Date> colProximoPlazo;
+
+    @FXML
+    private TableColumn<GastosPeriodicos, Date> colCantidadTotal1;
+
+    public void initialize() {
+        colCantidadTotal.setCellValueFactory(new PropertyValueFactory<>("cantidadTotal"));
+        colCantidadPagada.setCellValueFactory(new PropertyValueFactory<>("cantidadPagada"));
+        colCantidadRestante.setCellValueFactory(new PropertyValueFactory<>("cantidadRestante"));
+        colDineroPlazo.setCellValueFactory(new PropertyValueFactory<>("plazosDinero"));
+        colProximoPlazo.setCellValueFactory(new PropertyValueFactory<>("fechaSiguientePago"));
+        colCantidadTotal1.setCellValueFactory(new PropertyValueFactory<>("tipo"));
+
+        ObservableList<GastosPeriodicos> PagosRecurrentesObservable = FXCollections.observableArrayList(cuenta.getListaGastos());
+        tblPagosRecorrentes.setItems(PagosRecurrentesObservable);
+
+        cmbPlazos.getItems().addAll("Meses", "Años");
+        cmbPlazos.getSelectionModel().selectFirst();
+
+        cmbTipo.getItems().addAll("Prestamo", "Hipoteca");
+        cmbTipo.getSelectionModel().selectFirst();
+    }
+
+    @FXML
+    public void CalcularPagoMensual() {
+        if (txtCantidadTotal.getText().isEmpty() || txtInteresAnual.getText().isEmpty() ||
+            txtPlazos.getText().isEmpty()) {
+
+                txtPlazosDinero.setText("0.0");
+
+            return;
+        }
+ 
+        Double total = Double.parseDouble(txtCantidadTotal.getText());
+        Double interesAnual = Double.parseDouble(txtInteresAnual.getText());
+        int plazos = Integer.parseInt(txtPlazos.getText());
+        String mesesOAños = cmbPlazos.getValue();
+
+        // Convertir la tasa de interés anual a mensual
+        Double interesMensual = interesAnual / 12 / 100;
+        
+        if (mesesOAños.equals("Años")) {
+            plazos *= 12; // Convertir años a meses
+        }
+        
+        Double pagoMensual = total * (interesMensual * Math.pow(1 + interesMensual, plazos)) / (Math.pow(1 + interesMensual, plazos) - 1);
+        String pagoMensualFormateado = String.format("%.2f", pagoMensual);
+        pagoMensualFormateado = pagoMensualFormateado.replace(',', '.'); // Reemplazar coma con punto
+        txtPlazosDinero.setText(pagoMensualFormateado);
+    }
+
+    @FXML
+    public void NuevoPagoMensual() {
+        if (txtCantidadTotal.getText().isEmpty() || txtInteresAnual.getText().isEmpty() ||
+            txtPlazos.getText().isEmpty() || dtpFechaInicio.getValue() == null ||
+            txtPlazosDinero.getText().isEmpty() || txtPlazosDinero.getText().equals("0.0")) {
+            
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText(null);
+            alert.setContentText("Tienes que rellenar todos los campos.");
+            alert.showAndWait();
+
+            return;
+        }
+
+        Double total = Double.parseDouble(txtCantidadTotal.getText());
+        Double interesAnual = Double.parseDouble(txtInteresAnual.getText());
+        int plazos = Integer.parseInt(txtPlazos.getText());
+        String mesesOAños = cmbPlazos.getValue();
+        Date fechaInicio = java.sql.Date.valueOf(dtpFechaInicio.getValue());
+        Double PlazosPorDinero = Double.parseDouble(txtPlazosDinero.getText());
+        String tipo = cmbTipo.getValue();
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(fechaInicio);
+        calendar.add(Calendar.MONTH, 1); // Agregar un mes a la fecha de inicio
+        Date fechaSiguientePago = calendar.getTime();
+
+        if (mesesOAños.equals("Años")) {
+            plazos *= 12; // Convertir años a meses
+        }
+
+        GastosPeriodicos gp = new GastosPeriodicos(total, fechaInicio, tipo, fechaSiguientePago, PlazosPorDinero, interesAnual, plazos);
+
+        cuenta.getListaGastos().add(gp);
+        usuario.actualizarCuenta(cuenta);
+
+        ControladorMongoDB controlMongo = new ControladorMongoDB();
+        controlMongo.ActualizarUsuario(usuario);
+
+        App.setCuenta(cuenta);
+        App.setUsuario(usuario);
+
+        tblPagosRecorrentes.getItems().setAll(cuenta.getListaGastos());
+    }
 }
--- src/main/java/com/jovellanos/modelo/GastosPeriodicos.java
+++ src/main/java/com/jovellanos/modelo/GastosPeriodicos.java
@@ -5,23 +5,29 @@ import java.util.Date;
 public class GastosPeriodicos {
     private Double cantidadTotal;
     private Double cantidadPagada;
+    private Double cantidadRestante;
     private Date fechaInicio;
     private Date fechaSiguientePago;
-    private int plazosTiempo; // numero de dias entre cada plazo
-    private Double plazosDinero;
+    private Double plazosDinero; //Dinero por plazo
+    private Double interesAnual;
+    private int plazosTotales; // En meses
+    private String tipo;
 
     public GastosPeriodicos() {
 
     }
 
-    public GastosPeriodicos(Double cantidadTotal, Double cantidadPagada, Date fechaInicio,
-            Date fechaSiguientePago, int plazosTiempo, Double plazosDinero) {
+    public GastosPeriodicos(Double cantidadTotal, Date fechaInicio, String tipo,
+            Date fechaSiguientePago, Double plazosDinero, Double interesAnual, int plazosTotales) {
         this.cantidadTotal = cantidadTotal;
-        this.cantidadPagada = cantidadPagada;
+        this.cantidadPagada = 0.0;
+        this.cantidadRestante = cantidadTotal;
         this.fechaInicio = fechaInicio;
         this.fechaSiguientePago = fechaSiguientePago;
-        this.plazosTiempo = plazosTiempo;
         this.plazosDinero = plazosDinero;
+        this.interesAnual = interesAnual;
+        this.plazosTotales = plazosTotales;
+        this.tipo = tipo;
     }
 
     public Double getCantidadTotal() {
@@ -56,19 +62,43 @@ public class GastosPeriodicos {
         this.fechaSiguientePago = fechaSiguientePago;
     }
 
-    public int getPlazosTiempo() {
-        return plazosTiempo;
-    }
-
-    public void setPlazosTiempo(int plazosTiempo) {
-        this.plazosTiempo = plazosTiempo;
-    }
-
     public Double getPlazosDinero() {
         return plazosDinero;
     }
 
     public void setPlazosDinero(Double plazosDinero) {
         this.plazosDinero = plazosDinero;
+    }
+    
+    public Double getInteresAnual() {
+        return interesAnual;
+    }
+
+    public void setInteresAnual(Double interesAnual) {
+        this.interesAnual = interesAnual;
+    }
+
+    public int getPlazosTotales() {
+        return plazosTotales;
+    }
+
+    public void setPlazosTotales(int plazosTotales) {
+        this.plazosTotales = plazosTotales;
+    }
+
+    public Double getCantidadRestante() {
+        return cantidadRestante;
+    }
+
+    public void setCantidadRestante(Double cantidadRestante) {
+        this.cantidadRestante = cantidadRestante;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
     }
 }
