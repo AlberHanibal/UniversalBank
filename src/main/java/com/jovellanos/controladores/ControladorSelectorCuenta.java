3e169b3a628e024c03929c9103c947f5a4e7056c
Santiago <Santiago-Trabajo>
2024-05-27 13:00:01 +0200

    Borrar cuenta (Con confirmación)

--- src/main/java/com/jovellanos/controladores/ControladorSelectorCuenta.java
+++ src/main/java/com/jovellanos/controladores/ControladorSelectorCuenta.java
@@ -1,6 +1,7 @@
 package com.jovellanos.controladores;
 
 import java.util.ArrayList;
+import java.util.Optional;
 import java.util.UUID;
 
 import com.jovellanos.App;
@@ -11,9 +12,15 @@ import com.jovellanos.modelo.Usuario;
 import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.Node;
 
 public class ControladorSelectorCuenta {
@@ -46,39 +53,41 @@ public class ControladorSelectorCuenta {
         if (children.size() > 1) {
             children.remove(1, children.size());
         }
-
+    
         ArrayList<Cuenta> listaCuentas = usuario.getListaCuentas();
         int cont = 0;
-
+    
         int numColumns = 2; // Número deseado de columnas
-
+    
         double columnSpacing = 50;
         double anchorPaneWidth = 1200;
-
+    
         double totalColumnsWidth = numColumns * 300 + (numColumns - 1) * columnSpacing;
         double emptySpace = anchorPaneWidth - totalColumnsWidth;
         double leftMargin = emptySpace / 2;
-
+    
+        GridPane gridPane = new GridPane();
+        gridPane.setHgap(columnSpacing);
+        gridPane.setVgap(20); // Ajustar el espaciado vertical entre las filas
+    
         for (Cuenta cuenta : listaCuentas) {
             int id = cuenta.getId();
             Double balance = cuenta.getBalance();
-
+    
             Label label = new Label("ID:  " + id + "\nBalance: " + balance);
             label.setMinWidth(300);
             label.setMaxWidth(300);
-            //label.setStyle("-fx-alignment: CENTER-LEFT; " + "-fx-background-color: white; " + "-fx-padding: 10px;");
             label.getStyleClass().addAll("card", "cuenta");
-
-            anchorPane.getChildren().add(label);
-
-            int column = cont % numColumns; // Calculamos la fila y la columna que le toca a cada etiqueta.
-            int row = cont / numColumns;
-
-            double leftAnchor = leftMargin + column * (300 + columnSpacing);
-            double topAnchor = 50.0 + row * 100;
-            AnchorPane.setLeftAnchor(label, leftAnchor);
-            AnchorPane.setTopAnchor(label, topAnchor);
-
+    
+            Button btnEliminar = new Button("X");
+            btnEliminar.setOnAction(event -> eliminarCuenta(cuenta));
+    
+            HBox hbox = new HBox(label, btnEliminar);
+            hbox.setSpacing(10);
+            hbox.setAlignment(Pos.CENTER); // Alineación centrada horizontalmente
+    
+            gridPane.add(hbox, cont % numColumns, cont / numColumns);
+    
             label.setOnMouseClicked(new EventHandler<MouseEvent>() {
                 @Override
                 public void handle(MouseEvent event) {
@@ -86,8 +95,29 @@ public class ControladorSelectorCuenta {
                     App.getScene().setRoot(App.cargarEscena("fxml/principal/Principal.fxml"));
                 }
             });
-
+    
             cont++;
+        }
+        AnchorPane.setLeftAnchor(gridPane, leftMargin); // Centrar el GridPane en el AnchorPane
+        AnchorPane.setTopAnchor(gridPane, 70.0); // Ajustar el espacio superior
+    
+        anchorPane.getChildren().add(gridPane);
+    }
+
+    private void eliminarCuenta(Cuenta cuenta) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirmar Eliminación");
+        alert.setHeaderText(null);
+        alert.setContentText("¿Estás seguro de que quieres eliminar esta cuenta?");
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.isPresent() && result.get() == ButtonType.OK) {
+            usuario.getListaCuentas().remove(cuenta);
+
+            App.setUsuario(usuario);
+            controlMongo.ActualizarUsuario(usuario);
+
+            actualizarEtiquetas();
         }
     }
 
