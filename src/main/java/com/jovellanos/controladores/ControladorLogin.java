120e269044cc05983ae2faefd3734212f857ede4
Santiago <Santiago-Trabajo>
2024-05-28 11:25:59 +0200

    Añadidos datos predeterminados manuales

--- src/main/java/com/jovellanos/controladores/ControladorLogin.java
+++ src/main/java/com/jovellanos/controladores/ControladorLogin.java
@@ -1,7 +1,16 @@
 package com.jovellanos.controladores;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
 import com.jovellanos.App;
 import com.jovellanos.ControladorMongoDB;
+import com.jovellanos.modelo.Cuenta;
+import com.jovellanos.modelo.GastosPeriodicos;
+import com.jovellanos.modelo.Movimiento;
+import com.jovellanos.modelo.Tarjeta;
 import com.jovellanos.modelo.Usuario;
 
 import javafx.fxml.FXML;
@@ -12,6 +21,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 
 public class ControladorLogin {
+    ControladorMongoDB controlMongo = new ControladorMongoDB();
 
     @FXML
     private TextField txtUsuario;
@@ -28,6 +38,10 @@ public class ControladorLogin {
         if (App.getScene() != null) {
             App.getScene().getWindow().setWidth(600);
             App.getScene().getWindow().setHeight(400);
+        }
+
+        if (!controlMongo.ComprobarUsuario("Administrador")) {
+            CrearGrupoUsuarios();
         }
 
         txtUsuario.setOnKeyPressed(event -> {
@@ -94,5 +108,324 @@ public class ControladorLogin {
             txtContraseña.setVisible(false);
             txtContraseña.setManaged(false);
         }
+    }
+
+    private void CrearGrupoUsuarios() {
+        String username = "Usuario";
+        String contraseña = "Usuario";
+        String nombre = "Julián";
+        String apellidos = "Palacios García";
+        String direccion = "c/ De la Programación 14 2ºB";
+        String Telefono = "616190122";
+        String CorreoElectronico = "JulianPalacios@Gmail.es";
+
+        ArrayList<Cuenta> listaCuentas = new ArrayList<Cuenta>();
+
+        ArrayList<Tarjeta> listaTarjetas = CrearGrupoTarjetas1();
+        ArrayList<Movimiento> listaMovimientos = CrearGrupoMovimientos1();
+        ArrayList<GastosPeriodicos> listaGastos = CrearGrupoGastos1();
+
+        ArrayList<Tarjeta> listaTarjetas2 = new ArrayList<Tarjeta>();
+        ArrayList<Movimiento> listaMovimientos2 = CrearGrupoMovimientos2();
+        ArrayList<GastosPeriodicos> listaGastos2 = new ArrayList<GastosPeriodicos>();
+
+        ArrayList<Tarjeta> listaTarjetas3 = CrearGrupoTarjetas3();
+        ArrayList<Movimiento> listaMovimientos3 = CrearGrupoMovimientos3();
+        ArrayList<GastosPeriodicos> listaGastos3 = new ArrayList<GastosPeriodicos>();
+
+        Cuenta cuenta1 = new Cuenta(673932532, 15310.2, listaTarjetas, listaMovimientos, listaGastos);
+        Cuenta cuenta2 = new Cuenta(489301204, 450.99, listaTarjetas2, listaMovimientos2, listaGastos2);
+        Cuenta cuenta3 = new Cuenta(062373263, 2100.0, listaTarjetas3, listaMovimientos3, listaGastos3);
+        
+        listaCuentas.add(cuenta1);
+        listaCuentas.add(cuenta2);
+        listaCuentas.add(cuenta3);
+
+        Usuario userAdmin = new Usuario(username, contraseña, listaCuentas, nombre, apellidos);
+
+        userAdmin.setDireccion(direccion);
+        userAdmin.setTelefono(Telefono);
+        userAdmin.setCorreoElectronico(CorreoElectronico);
+
+        username = "a";
+        contraseña = "a";
+
+        Usuario a = new Usuario(username, contraseña, listaCuentas, nombre, apellidos);
+
+        a.setDireccion(direccion);
+        a.setTelefono(Telefono);
+        a.setCorreoElectronico(CorreoElectronico);
+
+        controlMongo.guardarUsuario(userAdmin);
+        controlMongo.guardarUsuario(a);
+    }
+
+    private ArrayList<Movimiento> CrearGrupoMovimientos1() {
+        ArrayList<Movimiento> historialMovimientos = new ArrayList<>();
+        //---------------------Movimiento 1----------------------------------------
+        Movimiento movimiento = new Movimiento();
+        movimiento.setCantidad(5000.0);
+        movimiento.setAsunto("Abriendo la cuenta");
+        movimiento.setTipo("Ingreso");
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        try {
+            Date date = dateFormat.parse("14/10/2022");
+            movimiento.setFecha(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        
+        historialMovimientos.add(movimiento);
+
+        //---------------------Movimiento 2----------------------------------------
+        movimiento = new Movimiento();
+        movimiento.setCantidad(2500.0);
+        movimiento.setAsunto("Regalo de un amigo");
+        movimiento.setTipo("Ingreso");
+
+        try {
+            Date date = dateFormat.parse("1/2/2023");
+            movimiento.setFecha(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        
+        historialMovimientos.add(movimiento);
+
+        //---------------------Movimiento 3----------------------------------------
+        movimiento = new Movimiento();
+        movimiento.setCantidad(-390.5);
+        movimiento.setAsunto("Prestamo a pagar");
+        movimiento.setTipo("Pago");
+
+        try {
+            Date date = dateFormat.parse("20/6/2023");
+            movimiento.setFecha(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        
+        historialMovimientos.add(movimiento);
+
+        //---------------------Movimiento 4----------------------------------------
+        movimiento = new Movimiento();
+        movimiento.setCantidad(500.0);
+        movimiento.setAsunto("Premio recibido");
+        movimiento.setTipo("Ingreso");
+
+        try {
+            Date date = dateFormat.parse("25/6/2023");
+            movimiento.setFecha(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        
+        historialMovimientos.add(movimiento);
+
+        //---------------------Movimiento 5----------------------------------------
+        movimiento = new Movimiento();
+        movimiento.setCantidad(-506.69);
+        movimiento.setAsunto("Pago de la Hipoteca");
+        movimiento.setTipo("Hipoteca");
+
+        try {
+            Date date = dateFormat.parse("1/1/2024");
+            movimiento.setFecha(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        
+        historialMovimientos.add(movimiento);
+
+        //---------------------Movimiento 6----------------------------------------
+        movimiento = new Movimiento();
+        movimiento.setCantidad(-506.69);
+        movimiento.setAsunto("Pago de la Hipoteca");
+        movimiento.setTipo("Hipoteca");
+
+        try {
+            Date date = dateFormat.parse("1/2/2024");
+            movimiento.setFecha(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        
+        historialMovimientos.add(movimiento);
+
+        //---------------------Movimiento 7----------------------------------------
+        movimiento = new Movimiento();
+        movimiento.setCantidad(9500.0);
+        movimiento.setAsunto("Ganancias");
+        movimiento.setTipo("Ingreso");
+
+        try {
+            Date date = dateFormat.parse("16/4/2024");
+            movimiento.setFecha(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        
+        historialMovimientos.add(movimiento);
+
+        //---------------------Movimiento 8----------------------------------------
+        movimiento = new Movimiento();
+        movimiento.setCantidad(-286.12);
+        movimiento.setAsunto("Deuda a un amigo");
+        movimiento.setTipo("Pago");
+
+        try {
+            Date date = dateFormat.parse("27/5/2024");
+            movimiento.setFecha(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        
+        historialMovimientos.add(movimiento);
+
+        return historialMovimientos;
+    }
+
+    private ArrayList<Movimiento> CrearGrupoMovimientos2() {
+        ArrayList<Movimiento> historialMovimientos = new ArrayList<>();
+        //---------------------Movimiento 1----------------------------------------
+        Movimiento movimiento = new Movimiento();
+        movimiento.setCantidad(1000.0);
+        movimiento.setAsunto("Abriendo la cuenta");
+        movimiento.setTipo("Ingreso");
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        try {
+            Date date = dateFormat.parse("7/2/2024");
+            movimiento.setFecha(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        
+        historialMovimientos.add(movimiento);
+
+        //---------------------Movimiento 2----------------------------------------
+        movimiento = new Movimiento();
+        movimiento.setCantidad(330.01);
+        movimiento.setAsunto("Compra en tienda");
+        movimiento.setTipo("Pago");
+
+        try {
+            Date date = dateFormat.parse("11/3/2024");
+            movimiento.setFecha(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        
+        historialMovimientos.add(movimiento);
+
+        //---------------------Movimiento 3----------------------------------------
+        movimiento = new Movimiento();
+        movimiento.setCantidad(-219.0);
+        movimiento.setAsunto("Compra en tienda");
+        movimiento.setTipo("Pago");
+
+        try {
+            Date date = dateFormat.parse("29/3/2023");
+            movimiento.setFecha(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        
+        historialMovimientos.add(movimiento);
+
+        return historialMovimientos;
+    }
+
+    private ArrayList<Movimiento> CrearGrupoMovimientos3() {
+        ArrayList<Movimiento> historialMovimientos = new ArrayList<>();
+        //---------------------Movimiento 1----------------------------------------
+        Movimiento movimiento = new Movimiento();
+        movimiento.setCantidad(2100.0);
+        movimiento.setAsunto("Abriendo la cuenta");
+        movimiento.setTipo("Ingreso");
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        try {
+            Date date = dateFormat.parse("12/5/2024");
+            movimiento.setFecha(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        
+        historialMovimientos.add(movimiento);
+
+        return historialMovimientos;
+    }
+
+    private ArrayList<Tarjeta> CrearGrupoTarjetas1() {
+        ArrayList<Tarjeta> listaTarjetas = new ArrayList<>();
+        //---------------------Tarjeta 1----------------------------------------
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        try {
+            Date date = dateFormat.parse("25/8/2026");
+            Tarjeta nuevaTarjeta = new Tarjeta(532910252, "Credito", "4728-1250-0192-2495", "831", 500.0, "8102", date);
+
+            listaTarjetas.add(nuevaTarjeta);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        //---------------------Tarjeta 2----------------------------------------
+        try {
+            Date date = dateFormat.parse("15/3/2025");
+            Tarjeta nuevaTarjeta = new Tarjeta(216399039, "Credito", "3284-0924-8432-2285", "188", 200.0, "1395", date);
+            
+            listaTarjetas.add(nuevaTarjeta);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        //---------------------Tarjeta 3----------------------------------------
+        try {
+            Date date = dateFormat.parse("1/1/2025");
+            Tarjeta nuevaTarjeta = new Tarjeta(593010298, "Debito", "0992-1374-6431-0192", "741", 250.0, "9412", date);
+            
+            listaTarjetas.add(nuevaTarjeta);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        
+        return listaTarjetas;
+    }
+
+    private ArrayList<Tarjeta> CrearGrupoTarjetas3() {
+        ArrayList<Tarjeta> listaTarjetas = new ArrayList<>();
+        //---------------------Tarjeta 1----------------------------------------
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        try {
+            Date date = dateFormat.parse("21/3/2026");
+            Tarjeta nuevaTarjeta = new Tarjeta(68301002, "Credito", "2860-2012-2068-9175", "108", 500.0, "9376", date);
+
+            listaTarjetas.add(nuevaTarjeta);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return listaTarjetas;
+    }
+
+    private ArrayList<GastosPeriodicos> CrearGrupoGastos1() {
+        ArrayList<GastosPeriodicos> listaGastos = new ArrayList<>();
+        //---------------------Gasto 1----------------------------------------
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        try {
+            Date fechaInicio = dateFormat.parse("1/1/2024");
+            Date fechaSiguientePago = dateFormat.parse("1/6/2024");
+
+            GastosPeriodicos gastoPeriodico = new GastosPeriodicos(100000.0, fechaInicio, "Hipoteca", fechaSiguientePago, 506.69, 4.5, 360);
+
+            listaGastos.add(gastoPeriodico);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        
+        return listaGastos;
     }
 }
--- src/main/java/com/jovellanos/controladores/principal/ControladorPrincipal.java
+++ src/main/java/com/jovellanos/controladores/principal/ControladorPrincipal.java
@@ -3,14 +3,11 @@ package com.jovellanos.controladores.principal;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.Random;
 
 import com.jovellanos.App;
 import com.jovellanos.ControladorMongoDB;
 import com.jovellanos.modelo.Cuenta;
 import com.jovellanos.modelo.GastosPeriodicos;
-import com.jovellanos.modelo.Movimiento;
-import com.jovellanos.modelo.Tarjeta;
 import com.jovellanos.modelo.Usuario;
 
 import javafx.fxml.FXML;
@@ -47,25 +44,6 @@ public class ControladorPrincipal {
         App.getScene().getWindow().setHeight(750);
         App.setSubSceneCentral(sceneCentral);
 
-        for (Cuenta c : usuario.getListaCuentas()) {
-            if (c.getHistorialMovimientos().isEmpty()) {
-                c.setHistorialMovimientos(crearMovimientos()); // Codigo temporal para generar movimientos falsos
-                c.setListaTarjetas(crearTarjetas()); // Codigo temporal para generar tarjetas falsas (Pueden salir 0)
-
-                double balance = 0.0;
-
-                for (Movimiento m : c.getHistorialMovimientos()) {
-                    balance += m.getCantidad();
-                }
-
-                balance = Math.round(balance * 100.0) / 100.0;
-                c.setBalance(balance);
-                usuario.actualizarCuenta(c);
-
-                App.setUsuario(usuario);
-                controlMongo.ActualizarUsuario(usuario);
-            }
-        }
         comprobarPagos();
         cambiarCentral("fxml/principal/ResumenCuenta.fxml");
     }
@@ -145,106 +123,5 @@ public class ControladorPrincipal {
             }
         }
         controlMongo.ActualizarUsuario(usuario);
-    }
-
-    private static ArrayList<Movimiento> crearMovimientos() { // Codigo temporal para mostrar datos en la tabla movimientos ---------------------
-        ArrayList<Movimiento> historialMovimientos = new ArrayList<>();
-
-        Random random = new Random();
-        int num = random.nextInt(10) + 3;
-
-        for (int i = 0; i < num; i++) {
-            Movimiento movimiento = new Movimiento();
-
-            double cantidad = random.nextDouble() * 15000 - 1000; 
-            cantidad = Math.round(cantidad * 100.0) / 100.0;
-
-            movimiento.setCantidad(cantidad);
-            movimiento.setAsunto("Asunto " + (i + 1));
-            movimiento.setFecha(new Date());
-            movimiento.setTipo("Tipo " + (i + 1));
-            historialMovimientos.add(movimiento);
-        }
-
-        return historialMovimientos;
-    }
-
-    private static ArrayList<Tarjeta> crearTarjetas() { // Codigo temporal para generar Tarjetas -------------------------------------------------
-        ArrayList<Tarjeta> listaTarjetas = new ArrayList<>();
-    
-        Random random = new Random();
-        int num = random.nextInt(5);
-    
-        // Array de tipos de tarjetas disponibles
-        String[] tiposTarjeta = {"Crédito", "Débito", "Prepago", "Viaje", "Recompensas", "Corporativa"};
-    
-        for (int i = 0; i < num; i++) {
-            int id = i + random.nextInt(1000000000);
-
-            String tipo = tiposTarjeta[random.nextInt(tiposTarjeta.length)];
-
-            String NumeroTarjeta = generateCreditCardNumber();
-
-            String CVV = String.format("%03d", random.nextInt(1000));
-
-            double limiteDiario = random.nextDouble() * 2000;
-            limiteDiario = Math.round(limiteDiario * 100.0) / 100.0;
-
-            String pin = generateRandomPin();
-
-            Calendar cal = Calendar.getInstance();
-            cal.add(Calendar.YEAR, random.nextInt(5) + 1); // Fecha de caducidad aleatoria, entre 1 y 5 años desde la fecha actual
-            Date fechaCaducidad = cal.getTime();
-    
-            Tarjeta tarjeta = new Tarjeta(id, tipo, NumeroTarjeta, CVV, limiteDiario, pin, fechaCaducidad);
-            listaTarjetas.add(tarjeta);
-        }
-    
-        return listaTarjetas;
-    }
-
-    private static String generateRandomPin() {
-        Random random = new Random();
-        StringBuilder pinBuilder = new StringBuilder();
-
-        // Genera un PIN de 4 dígitos
-        for (int i = 0; i < 4; i++) {
-            pinBuilder.append(random.nextInt(10));
-        }
-
-        return pinBuilder.toString();
-    }
-
-    private static String generateCreditCardNumber() {
-        Random random = new Random();
-        StringBuilder cardNumberBuilder = new StringBuilder();
-    
-        // Genera los primeros 15 dígitos (números aleatorios)
-        for (int i = 0; i < 16; i++) {
-            if (i > 0 && i % 4 == 0) { // Agrega un guion después de cada grupo de cuatro dígitos
-                cardNumberBuilder.append("-");
-            }
-            cardNumberBuilder.append(random.nextInt(10));
-        }
-    
-        // Agrega un dígito de verificación utilizando el algoritmo de Luhn
-        String partialCardNumber = cardNumberBuilder.toString();
-        int sum = 0;
-        boolean doubleDigit = false;
-        for (int i = partialCardNumber.length() - 1; i >= 0; i--) {
-            int digit = Character.getNumericValue(partialCardNumber.charAt(i));
-            if (doubleDigit) {
-                digit *= 2;
-                if (digit > 9) {
-                    digit -= 9;
-                }
-            }
-            sum += digit;
-            doubleDigit = !doubleDigit;
-        }
-        int checkDigit = (10 - (sum % 10)) % 10;
-        cardNumberBuilder.append(checkDigit);
-    
-        return cardNumberBuilder.toString();
     }
 }
